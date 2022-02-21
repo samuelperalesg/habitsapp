fd069e0d012eafeba73a040f1e498df734a9470c
samuelperalesg <91507521+samuelperalesg@users.noreply.github.com>
2022-02-21 15:00:08 -0700

    wip: add dashboard and implement temporary habit-model

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
 	path('', views.welcome, name='welcome'),
+	path('signup', views.signup, name='signup'),
+	path('dashboard', views.dashboard, name='dashboard'),
+	path('inspo', views.inspo, name='inspo'),
+	path('add_edit', views.add_edit, name='add_edit'),
+	path('logout', views.logout, name='logout'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Views
+# ========================================================
 
 # Welcome View
 def welcome(request):
 	return render(request, 'welcome.html')
+
+# Signup View
+def signup(request):
+	return render(request, 'signup.html')
+
+# Dashboard View
+def dashboard(request):
+	return render(request, 'dashboard.html', {'habits': habits})
+
+# Inspo View
+def inspo(request):
+	return render(request, 'inspo.html')
+
+# Add_edit View
+def add_edit(request):
+	return render(request, 'add_edit.html')
+
+# Logout View
+def logout(request):
+	return render(request, 'logout.html')
+
+# Models
+# ========================================================
+
+class Habit:
+	def __init__(self, habit_name, healthy, plan_of_action, external_cue, internal_cue):
+		self.habit_name = habit_name
+		self.healthy = healthy
+		self.plan_of_action = plan_of_action
+		self.external_cue = external_cue
+		self.internal_cue = internal_cue
+
+habits = [
+	Habit('Brushing Teeth', 'True', 'none', 'Teeth feel dirty', 'I want to have a pretty smile'),
+]
