3dadb828ec021d6f2e5f4f19d41e7a3c2f1d7937
samuelperalesg <91507521+samuelperalesg@users.noreply.github.com>
2022-02-22 08:56:18 -0700

    wip: created Habit model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Habit Model
+
+class Habit(models.Model):
+	habit_name = models.CharField(max_length=100)
+	healthy = models.BooleanField()
+	plan_of_action = models.TextField(max_length=250)
+	external_cue = models.CharField(max_length=100)
+	internal_cue = models.CharField(max_length=100)
