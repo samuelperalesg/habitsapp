254466c0f85f2eebb11ab28f9fd6bfbcb506133f
samuelperalesg <91507521+samuelperalesg@users.noreply.github.com>
2023-12-18 19:06:40 -0700

    hosting: fixed allowed hosts

--- habitsapp/settings.py
+++ habitsapp/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'habitsapp.urls'
@@ -124,6 +125,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/dashboard'
 
