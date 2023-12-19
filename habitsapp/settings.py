ceeaa7c70b45f74d343c91f6083be8b2f8e48400
samuelperalesg <91507521+samuelperalesg@users.noreply.github.com>
2023-12-18 17:09:31 -0700

    hosting: changing exposed variables

--- habitsapp/settings.py
+++ habitsapp/settings.py
@@ -20,10 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xgbyamam&*@$syqt6_2*c+qtd6vtlmd%5clhj=%2#qy4_7+yyz'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
